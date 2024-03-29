5dfe8826a3879bfae2b4362fe3df6281e86134e1
V4ldum <56064897+V4ldum@users.noreply.github.com>
2024-03-29 23:40:39 +0100

    rustlings

---
+++ rustlings/exercises/enums/enums1.rs
@@ -0,0 +1,20 @@
+// enums1.rs
+//
+// No hints this time! ;)
+
+
+#[derive(Debug)]
+enum Message {
+    // define a few types of messages as used below
+    Quit,
+    Echo,
+    Move,
+    ChangeColor,
+}
+
+fn main() {
+    println!("{:?}", Message::Quit);
+    println!("{:?}", Message::Echo);
+    println!("{:?}", Message::Move);
+    println!("{:?}", Message::ChangeColor);
+}
