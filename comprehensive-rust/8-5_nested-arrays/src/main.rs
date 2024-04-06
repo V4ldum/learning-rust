57a13e9602a4c5c7466390a634bcdae18a2f6695
V4ldum <56064897+V4ldum@users.noreply.github.com>
2024-04-06 18:20:42 +0200

    nested arrays

---
+++ comprehensive-rust/8-5_nested-arrays/src/main.rs
@@ -0,0 +1,42 @@
+
+fn transpose(matrix: [[i32; 3]; 3]) -> [[i32; 3]; 3] {
+    let mut output= [[0; 3]; 3];
+
+    for i in 0..3 {
+        for j in 0..3 {
+            output[i][j] = matrix[j][i];
+        }
+    }
+
+    output
+}
+
+fn main() {
+    let matrix = [
+        [101, 102, 103], // <-- the comment makes rustfmt add a newline
+        [201, 202, 203],
+        [301, 302, 303],
+    ];
+
+    println!("matrix: {:#?}", matrix);
+    let transposed = transpose(matrix);
+    println!("transposed: {:#?}", transposed);
+}
+
+#[test]
+fn test_transpose() {
+    let matrix = [
+        [101, 102, 103], //
+        [201, 202, 203],
+        [301, 302, 303],
+    ];
+    let transposed = transpose(matrix);
+    assert_eq!(
+        transposed,
+        [
+            [101, 201, 301], //
+            [102, 202, 302],
+            [103, 203, 303],
+        ]
+    );
+}
